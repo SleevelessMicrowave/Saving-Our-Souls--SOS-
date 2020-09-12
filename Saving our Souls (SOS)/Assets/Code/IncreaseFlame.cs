7bdd396119f304b4a9614a18cac522d06c3a1741
ab274 <abpulavarthi@gmail.com>
2020-09-12 14:21:31 -0400

    Fire and Sounds
    Made fire animation and found sounds

---
+++ Saving our Souls (SOS)/Assets/Code/IncreaseFlame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseFlame : MonoBehaviour 
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+
+        //yield on a new YieldInstruction that waits for 30 seconds.
+        for(int i = 0; i<6; i++)
+        {
+            yield return new WaitForSeconds(15);
+            Instantiate(myPrefab, new Vector3(0,0,0), Quaternion.identity);
+        }
+
+    }
+}
---
+++ Saving our Souls (SOS)/Assets/Code/WakingUpSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WakingUpSound : MonoBehaviour
+{
+    public AudioSource source;
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    
+    void Update() 
+    {
+        // play the sound
+        source.Play();
+    }
+
+}
