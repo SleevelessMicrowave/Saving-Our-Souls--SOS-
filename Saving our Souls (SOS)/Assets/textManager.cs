92d3c402c186ead2eb26f5bedbb21abcc21a50fb
emluo <emluo@ctemc.org>
2020-09-11 21:47:39 -0400

    Question1 (Wallet)

---
+++ Saving our Souls (SOS)/Assets/textManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class textManager : MonoBehaviour
+{
+    public TMP_Text text;
+    public Button firstOp;
+    public Button secondOp;
+    
+
+    public bool option1;
+    public bool option2;
+
+    private enum States
+    {
+        question, trueState, falseState 
+    };
+
+    private States myState;
+
+    void Start ()
+    {
+        myState = States.question;
+    }
+
+    void Update ()
+    {
+        if (myState == States.question)
+        {
+            Question();
+        }
+        else if (myState == States.trueState)
+        {
+            TrueState();
+        }
+        else if (myState == States.falseState)
+        {
+            FalseState();
+        }
+    }
+
+    void OnEnable()
+    {
+        firstOp.onClick.AddListener(delegate
+        {
+            option1 = true;
+        });
+
+        secondOp.onClick.AddListener(delegate
+        {
+            option2 = true;
+        });
+    }
+
+    void Question()
+    {
+        text.text = "Look for wallet or Don’t look for wallet?";
+
+        if (option1 == true)
+        {
+            myState = States.trueState;
+        }
+        else if (option2 == true)
+        {
+            myState = States.falseState;
+        }
+    }
+
+
+    void TrueState()
+    {
+        text.text = "30 Time Reduction";
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void FalseState()
+    {
+        text.text = "";
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    
+
+}
