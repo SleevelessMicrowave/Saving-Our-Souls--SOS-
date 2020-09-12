98a57f6fad32acfc8ffebe2ecda5d67ab39bcc45
emluo <emluo@ctemc.org>
2020-09-12 11:33:43 -0400

    Update ScaleTween.cs

--- Saving our Souls (SOS)/Assets/ScaleTween.cs
+++ Saving our Souls (SOS)/Assets/ScaleTween.cs
@@ -7,7 +7,7 @@ public class ScaleTween : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LeanTween.scale()
     }
 
     // Update is called once per frame
